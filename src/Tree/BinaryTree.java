0e57e92fcbb393de39272c77d90e633e00e6a262
Tom C Simon <tomcsimon01@gmail.com>
2026-07-01 10:50:39 +0530

    implement other methods in binary tree

--- src/Tree/BinaryTree.java
+++ src/Tree/BinaryTree.java
@@ -1,23 +1,35 @@
 package Tree;
 
+import static java.lang.Integer.*;
+
 public class BinaryTree {
 
     public static void main(String[] args) {
 
         Tree tree = new Tree();
 
-        tree.root = new Node("R-10");
-        tree.root.left = new Node("l-100");
-        tree.root.right = new Node("r-50");
-        tree.root.left.left = new Node("l-40");
-        tree.root.left.right = new Node("r-20");
-        tree.root.left.left.left = new Node("l-30");
-        
-        tree.preOrder(tree.root);
+        tree.root = new Node(10);
+        tree.root.left = new Node(100);
+        tree.root.right = new Node(50);
+        tree.root.left.left = new Node(40);
+        tree.root.left.right = new Node(20);
+        tree.root.left.left.left = new Node(30);
+        tree.root.left.left.left.right = new Node(60);
+
+        tree.preOrder(tree.root);// 10 100 40 30 60 20 50
         System.out.println();
-        tree.inOrder(tree.root);
+        tree.inOrder(tree.root);// 30 60 40 100 20 10 50
         System.out.println();
-        tree.postOrder(tree.root);
+        tree.postOrder(tree.root);// 60 30 40 20 100 50 10
+        System.out.println();
+        System.out.println("Height: " + tree.height(tree.root));// 5
+        System.out.println("Count of Nodes: " + tree.count(tree.root)); // 7
+        System.out.println("Count of Leaf Nodes: " + tree.countOfLeafNodes(tree.root));// 3
+        System.out.println("Is l-40 in tree: " + tree.search(tree.root, 40));// true
+        System.out.println("Count of Internal Nodes: " + tree.countInternalNodes(tree.root));// 4
+        System.out.println("Largest Element: " + tree.findMax(tree.root));// 100
+        System.out.println("Largest Element: " + tree.findMin(tree.root));// 10
+        System.out.println("Count of Nodes with one  Child: " + tree.countNodesWithOneChild(tree.root));// 2
     }
 }
 
@@ -52,15 +64,80 @@ class Tree {
         postOrder(root.right);
         System.out.print(root.data + " ");
     }
+
+    public int height(Node root) {
+
+        if(root == null) return 0;
+        int leftHeight = 1 + height(root.left);
+        int rightHeight =  1 + height(root.right);
+        return Math.max(leftHeight, rightHeight);
+    }
+
+    public int count(Node root) {
+
+        if(root == null) return 0;
+        int left = count(root.left);
+        int right = count(root.right);
+        return 1 + left + right;
+    }
+
+    public int countOfLeafNodes(Node root) {
+
+        if(root == null) return 0;
+        if(root.left == null && root.right == null) return 1;
+        return countOfLeafNodes(root.left) + countOfLeafNodes(root.right);
+    }
+
+    public boolean search(Node root, int data) {
+
+        if(root == null) return false;
+        if(root.data == data) return true;
+        boolean flag =  search(root.left, data);
+        return (flag) ? true : search(root.right, data);
+    }
+
+    public int countInternalNodes(Node root) {
+
+        if(root == null) return 0;
+        if(root.left == null && root.right == null) return 0;
+        return 1 + countInternalNodes(root.left) + countInternalNodes(root.right);
+    }
+
+    public int findMax(Node root) {
+
+        if(root == null) return MIN_VALUE;
+        int leftMax = findMax(root.left);
+        int rightMax = findMax(root.right);
+        return Math.max(root.data, Math.max(leftMax, rightMax));
+    }
+
+    public int findMin(Node root) {
+
+        if(root == null) return MAX_VALUE;
+        int leftMin = findMin(root.left);
+        int rightMin = findMin(root.right);
+        return Math.min(root.data, Math.min(leftMin, rightMin));
+    }
+
+    public int countNodesWithOneChild(Node root) {
+
+        if(root == null) return 0;
+        int left = countNodesWithOneChild(root.left);
+        int right = countNodesWithOneChild(root.right);
+        if((root.left == null) != (root.right == null)) {
+            return 1 + left + right;
+        }
+        return left + right;
+    }
 }
 
 class Node {
 
-    String data;
+    int data;
     Node left;
     Node right;
 
-    public Node(String data) {
+    public Node(int data) {
         this.data = data;
         this.left = null;
         this.right = null;
